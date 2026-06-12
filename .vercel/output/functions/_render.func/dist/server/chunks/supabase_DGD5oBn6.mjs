573bba12d18e9e2123c1a8f7fe241ae3f58ef071
mdwipasha <mdwipasha13@gmail.com>
2026-06-12 14:00:11 +0700

    chore: remove unused package-lock.json dependency configuration

--- .vercel/output/functions/_render.func/dist/server/chunks/supabase_DGD5oBn6.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  "https://imhvcxwkpyajlcygncby.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltaHZjeHdrcHlhamxjeWduY2J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NDk2ODAsImV4cCI6MjA5NjAyNTY4MH0.SjxoQ8HolJkL518JOarl20Dhk1jKpyjNcxuH_0BdNII"
-);
-
-export { supabase as s };
--- .vercel/output/static/_astro/index.C4H-CXe2.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index.DAW2DOZk.js";var a=r();export{a as r};
